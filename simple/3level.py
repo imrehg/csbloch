06036f1b2d94bb0a9357c473583a87df59e24d27
unknown <Main@.(none)>
2009-11-13 16:48:13 +0800

    3level: clear unnecessary things, make it like a module

--- simple/3level.py
+++ simple/3level.py
@@ -13,31 +13,32 @@ import pylab as pl
 ## ---    2
 ##  1
 
-# Excited state lifetimes
-# Total should be 1, everything else is scaled with it
-G = 1.0
-G1 = 1
-G2 = G - G1
-# Rabi frequencies (untis of G)
-Om1 = 1
-Om2 = 1
-# Laser linewidths (units of G)
-gg1 = 0.05
-gg2 = gg1
-# Detuning (Units of G)
-d1 = 0
-d2 = 0
-# Starting density matrix elements
-# Matrix elements: x11, x22, x33, x12, x13, x23, y12, y13, y23
-# x11, x22, x33 (meaning: q0[0:3]) are the populations 
-q0 = sp.zeros((9,1))
-# Start in level 1
-q0[0] = 1
-
-# Make standard parameters variable
-laser1 = (Om1, d1, gg1)
-laser2 = (Om2, d2, gg2)
-params = (G,G1,G2,laser1,laser2)
+#### Example settings
+## Excited state lifetimes
+## Total should be 1, everything else is scaled with it
+#G = 1.0 * 2
+#G1 = 1 * G
+#G2 = G - G1
+## Rabi frequencies (untis of G)
+#Om1 = 10 * G
+#Om2 = 0 * G
+## Laser linewidths (units of G)
+#gg1 = 0 * G
+#gg2 = gg1
+## Detuning (Units of G)
+#d1 = 0
+#d2 = -1/8
+## Starting density matrix elements
+## Matrix elements: x11, x22, x33, x12, x13, x23, y12, y13, y23
+## x11, x22, x33 (meaning: q0[0:3]) are the populations 
+#q0 = sp.zeros((9,1))
+## Start in level 1
+#q0[0] = 1
+#
+## Make standard parameters variable
+#laser1 = (Om1, d1, gg1)
+#laser2 = (Om2, d2, gg2)
+#params = (G,G1,G2,laser1,laser2)
 
 def Mmat(params):
     ''' To evolve the density matrix
