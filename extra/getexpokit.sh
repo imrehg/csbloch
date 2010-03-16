fda11cf87ac3816a55b2cfbcab87537d55ed7b82
Gergely Imreh <imrehg@gmail.com>
2010-03-16 17:28:11 +0800

    getexpokit: download, patch and compile expokit for python (f2py)
    needs fortran compiler, blas, lapack, internet connection

---
+++ extra/getexpokit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+wget http://www.maths.uq.edu.au/expokit/expokit.tar.gz
+tar -xzvf expokit.tar.gz
+cd expokit/fortran
+
+
+# Based on: http://sf.anu.edu.au/~mhk900/Python_Workshop/short.pdf
+# Steps
+# 1) Update references to matve
+patch -p 0 < ../../expokit.f.patch
+
+# 2) Generate header
+f2py -m expokit -h expokit.pyf expokit.f
+
+# 3) Patch header
+patch -p 0 < ../../expokit.pyf.patch
+
+# 4) Compile package
+f2py -c expokit.pyf expokit.f blas.f lapack.f
+
+# The shiny new "expokit.so" should be ready for you!
