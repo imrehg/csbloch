fd0608edab6f9ae49dbce6fc38b25741f2f8a06b
Gergely Imreh <imrehg@gmail.com>
2010-10-08 12:53:55 +0800

    3level_modelock: version that is know to work

--- Greg/3level_modelock.py
+++ Greg/3level_modelock.py
@@ -16,23 +16,26 @@ from expv import spexpv
 # ----(1) ----(2)
 
 # # Upper level lifetime, detunings and linewidths
+# Gamma is in anglular units - e.g. 1 MHz => 2pi x 1 MHz
 Gt = 1
 d13 = 0
-G31 = 0.9
+G31 = Gt * 0.9
 d23 = 0
 G32 = Gt - G31
 Gtot = G31 + G32
 
-om13real = 1
-om23real = 1
-d13real = 0
-d23real = 0
+om13real = 0.5
+om23real = 2
+d13real = -1.3
+d23real = -1.3
+start = 0
 
 bloch_atomic = ll_mat(9, 9, 10)
 bloch_atomic[0, 2] = G31
 bloch_atomic[1, 2] = G32
 bloch_atomic[2, 2] = -Gtot
-for i in xrange(4, 9):
+cdecay = [4, 5, 7, 8]
+for i in cdecay:
     bloch_atomic[i, i] = -Gtot / 2 
 
 laser1_pow = ll_mat(9, 9, 10)
@@ -47,6 +50,7 @@ laser1_pow[8, 3] = -1/2
 laser1_det = ll_mat(9, 9, 10)
 laser1_det[4, 7] = 1
 laser1_det[7, 4] = -1
+
 
 # print out
 # nt = 101
@@ -89,47 +93,149 @@ laser2_det = ll_mat(9, 9, 10)
 laser2_det[5, 8] = 1
 laser2_det[8, 5] = -1
 
+laser12_det = ll_mat(9, 9, 10)
+laser12_det[3, 6] = 1
+laser12_det[6, 3] = -1
 
-### Time Dependent
+
+
+# ### Time Dependent
 out = bloch_atomic.copy()
 out.shift(om13real, laser1_pow)
 out.shift(d13real, laser1_det)
 out.shift(om23real, laser2_pow)
 out.shift(d23real, laser2_det)
+out.shift(d13real-d23real, laser12_det)
 
-om13 = om13real
-om23 = om23real
-d12 = 0
-d13 = d13real
-d23 = d23real
-obm = array([[0, 0, G31, 0, 0, 0, 0, om13, 0],
-             [0, 0, G32, 0, 0, 0, 0, 0, om23],
-             [0, 0, -Gtot, 0, 0, 0, 0, -om13, -om23],
-             [0, 0, 0, 0, 0, 0, d12, om23/2, om13/2],
-             [0, 0, 0, 0, -Gtot/2, 0, om23/2, d13, 0],
-             [0, 0, 0, 0, 0, -Gtot/2, -om13/2, 0, d23],
-             [0, 0, 0, -d12, -om23/2, om13/2, -Gtot/2, 0, 0],
-             [-om13/2, 0, om13/2, -om23/2, -d13, 0, 0, -Gtot/2, 0],
-             [0, -om23/2, om23/2, -om13/2, 0, -d23, 0, 0, -Gtot/2]])
+print out
 
-nt = 201
-t = linspace(0, 20, nt)
-p = array(zeros((nt, 3)))
-p2 = array(zeros((nt, 3)))
+
+# dd = [0, 4, 8]
+# tr2 = {0: 0, 1: 3, 2: 4, 4: 1, 5: 5, 8: 2}
+# tr2i = {1:6, 2:7, 5:8}
+
+
+# tr1 = {0:0, 1:4, 2:8, 3:1, 4:2, 5:5}
+# tr1i = {6:1, 7:2, 8:5}
+# # conjugates
+# tr2c = {1:3, 2:6, 5:7}
+
+# cpm = zeros((9, 9), dtype=complex)
+
+# # Real parts effect on real parts
+# for i in tr1.keys():
+#     for k in tr1.keys():
+#         scale = 1 if (k < 3) else 1/2
+#         cpm[tr1[i], tr1[k]] += out[i, k] * scale
+# for i in tr1.keys():
+#     for k in tr1i.keys():
+#         cpm[tr1[i], tr1i[k]] += -1j*out[i, k]/2
+#         cpm[tr1[i], tr2c[tr1i[k]]] += 1j*out[i, k]/2
+# for i in tr1i.keys():
+#     for k in tr1.keys():
+#         cpm[tr1i[i], tr1[k]] += 1j*out[i, k]/2
+# for i in tr1i.keys():
+#     for k in tr1i.keys():
+#         cpm[tr1i[i], tr1i[k]] += out[i, k]/2
+
+# for i in tr2.keys():
+#     for k in tr2.keys():
+#         cpm[i, k] += out[tr2[i], tr2[k]]
+#         if tr2i.has_key(k):
+#             ipart = 1j*out[tr2[i], tr2i[k]]
+#             if i in dd:
+#                 ipart /= 2
+#             cpm[i, k] += ipart
+#             cpm[i, tr2c[k]] += ipart
+# for i in tr2.keys():
+#     for k in tr2i.keys():
+#         print tr2[i], tr2i[k], " -> ", i, k, " = ", out[tr2[i], tr2i[k]]
+#         cpm[i, k] += 1j*out[tr2[i], tr2i[k]]
+
+# for i in tr2i.keys():
+#     for k in tr2i.keys():
+#         cpm[i, k] += 1j*out[tr2i[i], tr2i[k]]
+# for i in tr2i.keys():
+#     for k in tr2.keys():
+#         cpm[i, k] += 1j*out[tr2i[i], tr2[k]]
+
+# Conjugates
+# for i in tr2c.keys():
+#     cpm[tr2c[i], tr2c[i]] = cpm[i, i].conjugate()
+
+# print real(cpm), "Real"
+# print
+# print imag(cpm), "Imaginary"
+# print out
+
+# for i in xrange(9):
+#     v = zeros(9)
+#     v[i] = 1
+#     w = empty(9)
+#     print "%d ----------- " %i
+#     print v
+#     out.matvec(v, w)
+#     print w
+#     print
+
+
+# om13 = om13real
+# om23 = om23real
+# d12 = d13real - d23real
+# d13 = d13real
+# d23 = d23real
+# obm = array([[0, 0, G31, 0, 0, 0, 0, om13, 0],
+#              [0, 0, G32, 0, 0, 0, 0, 0, om23],
+#              [0, 0, -Gtot, 0, 0, 0, 0, -om13, -om23],
+#              [0, 0, 0, 0, 0, 0, d12, om23/2, om13/2],
+#              [0, 0, 0, 0, -Gtot/2, 0, om23/2, d13, 0],
+#              [0, 0, 0, 0, 0, -Gtot/2, -om13/2, 0, d23],
+#              [0, 0, 0, -d12, -om23/2, om13/2, -Gtot/2, 0, 0],
+#              [-om13/2, 0, om13/2, -om23/2, -d13, 0, 0, -Gtot/2, 0],
+#              [0, -om23/2, om23/2, -om13/2, 0, -d23, 0, 0, -Gtot/2]])
+
+nt = 151
+t = linspace(0, 20,  nt)
+p = array(zeros((nt, 9)))
+p2 = array(zeros((nt, 9)))
 v = matrix(zeros((9, 1)))
-v[0] = 1
+v[start] = 1
 # print v
 for i in xrange(nt):
     vt, e, hump = spexpv(t[i], out, v)
-    p[i, 0:3] = vt[0:3]
-    v2 = dot(expm(obm*t[i]), v)
-    p2[i, 0:3] = v2[0:3].T
+    p[i, 0:9] = vt[0:9]
+    # p[i, 0:3] = vt[3:6]
+    # v2 = dot(expm(obm*t[i]), v)
+    # p2[i, 0:9] = v2[0:9].T
+    # p = p2
 for i in xrange(3):
-    pl.plot(t, p[:, i], label="(%d)"%(i+1))
+    pl.plot(t, p[:, i], '-', label="(%d)"%(i+1))
+# for i in xrange(3):
+#     pl.plot(t, p2[:, i], 'x', label="X(%d)"%(i+1))
+data = loadtxt('../matlab/compare.csv', delimiter=',')
 for i in xrange(3):
-    pl.plot(t, p2[:, i], 'x', label="X(%d)"%(i+1))
+    pl.plot(t, data[:, i], 'o', label="M(%d)"%(i+1))
 pl.legend(loc='best')
+pl.title('Populations')
+
+pl.figure()
+for i in xrange(3, 6):
+    pl.plot(t, p[:, i], label="(%d)"%(i+1))
+    pl.plot(t, data[:, i], 'o', label="M(%d)"%(i+1))
+pl.title('Real coherence')
+pl.legend(loc='best')
+
+pl.figure()
+for i in xrange(6, 9):
+    pl.plot(t, p[:, i], label="(%d)"%(i+1))
+    pl.plot(t, data[:, i], 'o', label="M(%d)"%(i+1))
+pl.title('Imaginary coherence')
+pl.legend(loc='best')
+
 pl.show()
+
+######################
+
 
 # print out
 # om13 = om13real
@@ -160,7 +266,9 @@ pl.show()
 #     out.shift(d23real, laser2_det)
 #     vt, e, hump = spexpv(100, out, v)
 #     fluo = append(fluo, vt[2])
-# pl.plot(dlist, fluo)
+# pl.plot(dlist, fluo, 'r-')
+# # data = loadtxt('matlab.csv', delimiter=',')
+# # pl.plot(data[:,0], data[:,1], 'k--')
 # pl.show()
 
 #     # x11, x22, x33, x12, x13, x23, y12, y13, y23
