c9b830d825ce5f1be32fb1718c41d519bd58cd53
Gergely Imreh <imrehg@gmail.com>
2010-10-18 16:38:01 +0800

    paperreproduction no. 2: Matthew McDonnell's thesis
    (well, a de facto benchmark)

---
+++ Greg/paperrepro02.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python
+"""
+Reproducing results from:
+Matthew McDonnell's thesis, 2003
+http://www.physics.ox.ac.uk/Users/iontrap/publications.html
+
+    ---- (3)
+    /  \
+   /   ----(2)
+----(1) 
+
+
+"""
+
+from __future__ import division
+from numpy import *
+from scipy import *
+from scipy.linalg import expm
+import pylab as pl
+from scipy.integrate import ode, quad
+from time import time, clock
+from pysparse.spmatrix import *
+from expv import spexpv
+
+def genmat(G):
+    """
+    genmat(G)
+    Generate the parts of the Bloch-equations to use in the
+    actual calculation
+
+    Input:
+    G = [G31, G32] the upper level decay rate to the two ground states
+
+    Output:
+    bloch_atomic, laser1_pow, laser1_det, laser2_pow, laser2_det, laser12_det
+    """
+    G31 = G[0]
+    G32 = G[1]
+    Gtot = G31 + G32
+
+    bloch_atomic = ll_mat(9, 9, 10)
+    bloch_atomic[0, 2] = G31
+    bloch_atomic[1, 2] = G32
+    bloch_atomic[2, 2] = -Gtot
+    cdecay = [4, 5, 7, 8]
+    for i in cdecay:
+        bloch_atomic[i, i] = -Gtot / 2 
+
+    laser1_pow = ll_mat(9, 9, 10)
+    laser1_pow[0, 7] = 1
+    laser1_pow[2, 7] = -1
+    laser1_pow[3, 8] = 1/2
+    laser1_pow[5, 6] = -1/2
+    laser1_pow[6, 5] = 1/2
+    laser1_pow[7, 0] = -1/2
+    laser1_pow[7, 2] = 1/2
+    laser1_pow[8, 3] = -1/2
+    laser1_det = ll_mat(9, 9, 10)
+    laser1_det[4, 7] = 1
+    laser1_det[7, 4] = -1
+
+    laser2_pow = ll_mat(9, 9, 10)
+    laser2_pow[1, 8] = 1
+    laser2_pow[2, 8] = -1
+    laser2_pow[3, 7] = 1/2
+    laser2_pow[4, 6] = 1/2
+    laser2_pow[6, 4] = -1/2
+    laser2_pow[7, 3] = -1/2
+    laser2_pow[8, 1] = -1/2
+    laser2_pow[8, 2] = 1/2
+    laser2_det = ll_mat(9, 9, 10)
+    laser2_det[5, 8] = 1
+    laser2_det[8, 5] = -1
+
+    laser12_det = ll_mat(9, 9, 10)
+    laser12_det[3, 6] = 1
+    laser12_det[6, 3] = -1
+
+    return bloch_atomic, laser1_pow, laser1_det, laser2_pow, laser2_det, laser12_det
+
+def timedomain(args):
+    """
+    Input:
+    mnames, t, v0, det13, det23, omega13, omega23
+
+    """
+    mnames, t, v0, det13, det23, omega13, omega23 = args
+    for name in mnames:
+        globals()[name] = ll_mat_from_mtx(name)
+    # detunings
+    d13real = det13
+    d23real = det23
+    d12real = d13real - d23real
+
+    # Between pulses: CW laser is on
+    bloch = bloch_atomic.copy()
+    bloch.shift(omega13, laser1_pow)
+    bloch.shift(omega23, laser2_pow)
+    bloch.shift(d13real, laser1_det)
+    bloch.shift(d23real, laser2_det)
+    bloch.shift(d12real, laser12_det)
+
+    results = zeros((9, len(t)))
+    vt = v0
+
+    for i in xrange(len(t)):        
+        vt, e, hump = spexpv(t[i], bloch, v0)
+        vt = matrix(vt).T
+        for k in range(0, 9):
+            results[k, i] = vt[k]
+    return results
+
+if __name__ == "__main__":
+
+    ## Setting up parameters
+    # scaling factor to get dimensionless time
+    Gt = 2*pi
+
+
+
+    # Figure 4.3, page 53
+    pl.figure()
+    G = ([Gt*1, Gt*0], [Gt*0.99, Gt*0.01], [Gt*0.9, Gt*0.10], [Gt*0.5, Gt*0.5])
+    for parami, Gi in enumerate(G):
+        # Generate the Bloch-equation matrices of different contributions
+        matrices = genmat(Gi)
+        mnames = ['bloch_atomic', 'laser1_pow', 'laser1_det', 'laser2_pow', 'laser2_det', 'laser12_det']
+        for i in range(len(mnames)):
+            matrices[i].export_mtx(mnames[i])
+
+        ## Starting parameters
+        v0 = matrix(zeros((9, 1)))
+        v0[0] = 1
+
+        det13 = 0
+        det23 = 0
+        omega13 = 1 * Gt
+        omega23 = 0 * Gt
+        t = linspace(0, 20/Gt, 201)
+        args = (mnames, t, v0, det13, det23, omega13, omega23)
+        res = timedomain(args)
+        t = t * Gt
+
+        pl.subplot(2, 2, parami+1)
+        pl.plot(t, res[0, :]*100, 'k-', label="|1>", linewidth=2)
+        pl.plot(t, res[1, :]*100, 'k--', label="|2>", linewidth=2)
+        pl.plot(t, res[2, :]*100, 'k:', label="|3>", linewidth=2)
+        pl.xlabel("Time (1/G)")
+        pl.ylabel("State population (%)")
+        pl.legend()
+
+    # Figure 4.5, page 55
+    pl.figure()
+    om23list = [1 * Gt, 2 * Gt, 4 * Gt, 8 * Gt ]
+    for parami, omega23 in enumerate(om23list):
+        G = [0.9 * Gt, 0.1 * Gt]
+        # Generate the Bloch-equation matrices of different contributions
+        matrices = genmat(G)
+        mnames = ['bloch_atomic', 'laser1_pow', 'laser1_det', 'laser2_pow', 'laser2_det', 'laser12_det']
+        for i in range(len(mnames)):
+            matrices[i].export_mtx(mnames[i])
+
+        ## Starting parameters
+        v0 = matrix(zeros((9, 1)))
+        v0[0] = 1
+
+        det13 = 0
+        det23 = 0
+        omega13 = 1 * Gt
+        t = linspace(0, 20/Gt, 201)
+        args = (mnames, t, v0, det13, det23, omega13, omega23)
+        res = timedomain(args)
+        t = t * Gt
+
+        pl.subplot(2, 2, parami+1)
+        pl.plot(t, res[0, :]*100, 'k-', label="|1>", linewidth=2)
+        pl.plot(t, res[1, :]*100, 'k--', label="|2>", linewidth=2)
+        pl.plot(t, res[2, :]*100, 'k:', label="|3>", linewidth=2)
+        pl.xlabel("Time (1/G)")
+        pl.ylabel("State population (%)")
+        pl.legend()
+
+    # Figure 4.6, page 56
+    pl.figure()
+    om23list = [1 * Gt, 2 * Gt, 4 * Gt, 8 * Gt ]
+    for parami, omega23 in enumerate(om23list):
+        G = [0.9 * Gt, 0.1 * Gt]
+        # Generate the Bloch-equation matrices of different contributions
+        matrices = genmat(G)
+        mnames = ['bloch_atomic', 'laser1_pow', 'laser1_det', 'laser2_pow', 'laser2_det', 'laser12_det']
+        for i in range(len(mnames)):
+            matrices[i].export_mtx(mnames[i])
+
+        ## Starting parameters
+        v0 = matrix(zeros((9, 1)))
+        v0[0] = 1
+
+        det23 = 0
+        omega13 = 1 * Gt
+        t = [0, 100 / Gt]
+        det13list = linspace(-10*Gt, 10*Gt, 401)
+        reslist  = array([])
+        for det13 in det13list:
+            args = (mnames, t, v0, det13, det23, omega13, omega23)
+            res = timedomain(args)
+            reslist = append(reslist, 100*res[2, -1])
+
+        pl.subplot(2, 2, parami+1)
+        pl.plot(det13list / Gt, reslist, 'k-', label="|3>", linewidth=2)
+        pl.ylim([0, max(reslist)*1.05])
+        pl.xlim([det13list[0] / Gt, det13list[-1] / Gt])
+        pl.xlabel("Time (1/G)")
+        pl.ylabel("|3> state population (%)")
+
+    pl.show()
