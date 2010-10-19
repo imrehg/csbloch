336c2e388e8dee0df1bc8329968ca20ff449f305
Gergely Imreh <imrehg@gmail.com>
2010-10-19 14:53:40 +0800

    paperrepro02: replace duplicate bloch matrix calculation with the unified one

--- Greg/paperrepro02.py
+++ Greg/paperrepro02.py
@@ -21,62 +21,7 @@ from scipy.integrate import ode, quad
 from time import time, clock
 from pysparse.spmatrix import *
 from expv import spexpv
-
-def genmat(G):
-    """
-    genmat(G)
-    Generate the parts of the Bloch-equations to use in the
-    actual calculation
-
-    Input:
-    G = [G31, G32] the upper level decay rate to the two ground states
-
-    Output:
-    bloch_atomic, laser1_pow, laser1_det, laser2_pow, laser2_det, laser12_det
-    """
-    G31 = G[0]
-    G32 = G[1]
-    Gtot = G31 + G32
-
-    bloch_atomic = ll_mat(9, 9, 10)
-    bloch_atomic[0, 2] = G31
-    bloch_atomic[1, 2] = G32
-    bloch_atomic[2, 2] = -Gtot
-    cdecay = [4, 5, 7, 8]
-    for i in cdecay:
-        bloch_atomic[i, i] = -Gtot / 2 
-
-    laser1_pow = ll_mat(9, 9, 10)
-    laser1_pow[0, 7] = 1
-    laser1_pow[2, 7] = -1
-    laser1_pow[3, 8] = 1/2
-    laser1_pow[5, 6] = -1/2
-    laser1_pow[6, 5] = 1/2
-    laser1_pow[7, 0] = -1/2
-    laser1_pow[7, 2] = 1/2
-    laser1_pow[8, 3] = -1/2
-    laser1_det = ll_mat(9, 9, 10)
-    laser1_det[4, 7] = 1
-    laser1_det[7, 4] = -1
-
-    laser2_pow = ll_mat(9, 9, 10)
-    laser2_pow[1, 8] = 1
-    laser2_pow[2, 8] = -1
-    laser2_pow[3, 7] = 1/2
-    laser2_pow[4, 6] = 1/2
-    laser2_pow[6, 4] = -1/2
-    laser2_pow[7, 3] = -1/2
-    laser2_pow[8, 1] = -1/2
-    laser2_pow[8, 2] = 1/2
-    laser2_det = ll_mat(9, 9, 10)
-    laser2_det[5, 8] = 1
-    laser2_det[8, 5] = -1
-
-    laser12_det = ll_mat(9, 9, 10)
-    laser12_det[3, 6] = 1
-    laser12_det[6, 3] = -1
-
-    return bloch_atomic, laser1_pow, laser1_det, laser2_pow, laser2_det, laser12_det
+from blochmatrix import *
 
 def timedomain(args):
     """
@@ -123,7 +68,7 @@ if __name__ == "__main__":
     G = ([Gt*1, Gt*0], [Gt*0.99, Gt*0.01], [Gt*0.9, Gt*0.10], [Gt*0.5, Gt*0.5])
     for parami, Gi in enumerate(G):
         # Generate the Bloch-equation matrices of different contributions
-        matrices = genmat(Gi)
+        matrices = blochlambda(Gi)
         mnames = ['bloch_atomic', 'laser1_pow', 'laser1_det', 'laser2_pow', 'laser2_det', 'laser12_det']
         for i in range(len(mnames)):
             matrices[i].export_mtx(mnames[i])
@@ -155,7 +100,7 @@ if __name__ == "__main__":
     for parami, omega23 in enumerate(om23list):
         G = [0.9 * Gt, 0.1 * Gt]
         # Generate the Bloch-equation matrices of different contributions
-        matrices = genmat(G)
+        matrices = blochlambda(G)
         mnames = ['bloch_atomic', 'laser1_pow', 'laser1_det', 'laser2_pow', 'laser2_det', 'laser12_det']
         for i in range(len(mnames)):
             matrices[i].export_mtx(mnames[i])
@@ -186,7 +131,7 @@ if __name__ == "__main__":
     for parami, omega23 in enumerate(om23list):
         G = [0.9 * Gt, 0.1 * Gt]
         # Generate the Bloch-equation matrices of different contributions
-        matrices = genmat(G)
+        matrices = blochlambda(G)
         mnames = ['bloch_atomic', 'laser1_pow', 'laser1_det', 'laser2_pow', 'laser2_det', 'laser12_det']
         for i in range(len(mnames)):
             matrices[i].export_mtx(mnames[i])
