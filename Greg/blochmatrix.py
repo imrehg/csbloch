blochmatrix: fix population leakage due to wrong coefficients
The lower level population gains were not scaled together with the
upper level broading. This should fix most of population leakage.

Any more leakage is likely due to numerical error.

Greg/blochmatrix.py
@@ -251,7 +251,7 @@ def blochd2(broad=None):
     for i in xrange(len(Fupper)):
         for f in xrange(len(Flower)):
             prob = (2*Jupper+1)*(2*Flower[f]+1)*quantum.sixj(Jupper, Jlower, 1, Flower[f], Fupper[i], I)**2
-            A[f, i+off] = prob
+            A[f, i+off] = prob*G[Fupper[i]]
 
     M = np.zeros((nlev, nlev))
     off = len(Flower)